bd138979b2599b8e8337fdbcf3d286e5e9de5944
leopipu <leo.piche@hec.ca>
2022-01-25 00:45:36 -0500

    o

---
+++ Exercises.py
@@ -0,0 +1,66 @@
+                                                        #exercise 1
+# def to_miles(km):
+#     km = km*0.6214
+#     return km 
+
+# mesure = float(input("Enter a mesure: "))
+# calcul = round(to_miles(mesure),2)
+# print(str(mesure) +" km equals " + str(calcul) + " miles.")
+
+                                                        #exercise 2
+# import string
+
+
+# def toTime(i):
+    
+#     seconds = i % 60
+#     left_min_heure_ensec = i - seconds
+#     left_min_heure_enmin = left_min_heure_ensec/60
+#     minutes = left_min_heure_enmin % 60
+#     left_heure_enmin = left_min_heure_enmin - minutes
+#     heures = left_heure_enmin/60
+#     string = str(heures) + " hours :: " + str(minutes) + " minutes :: " + str(seconds) + " seconds"
+#     return string
+
+# time = int(input("Enter a time in seconds: "))
+# conversion = toTime(time)
+# print(conversion)
+
+                                                        #exercise 3
+
+def numbers_stats():
+    x=[] 
+    for i in range(int(input("How many elements are in list : "))): 
+        x.append(int(input()))
+    cumu = 0
+    for o in range(len(x)):
+        cumu += x[o] 
+    average = cumu / len(x)
+    x.sort
+    max = x[(len(x)-1)]
+    min = x[0]
+
+    print(str(max))
+    print(str(min))
+    print(str(average))
+
+numbers_stats()
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+                                        
+
+
+
+
+
